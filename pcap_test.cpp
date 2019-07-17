de4cf4c2530857540798c8166033ec3a2bd978f5
Xvezda <xvezda@naver.com>
2019-07-16 22:46:28 -0700

    Add tcp/udp structure and wrapper

--- pcap_test.cpp
+++ pcap_test.cpp
@@ -9,6 +9,7 @@
 #include <pcap.h>
 
 #include <unistd.h>
+#include <arpa/inet.h>
 #include <linux/limits.h>
 
 #define PUSH(cursor, target, size) 				\
@@ -25,7 +26,28 @@ using std::printf;
 using std::strlen;
 using std::memcpy;
 
-struct ip_header {
+struct tcp_packet {
+	uint16_t sp;
+	uint16_t dp;
+	uint32_t seq;
+	uint32_t ack;
+	unsigned offset : 4;
+	unsigned reserved : 3;
+	unsigned control_flag : 9;
+	/*
+	uint16_t window_size;
+	uint16_t checksum;
+	uint16_t urgent_pointer;
+	uint8_t *data;
+	*/
+};
+typedef struct tcp_packet tcp_t;
+
+struct udp_packet {
+};
+typedef struct udp_packet udp_t;
+
+struct ip_packet {
 	unsigned version : 4;
 	unsigned header_length : 4;
 	uint8_t type;
@@ -38,10 +60,12 @@ struct ip_header {
 	uint16_t checksum;
 	uint8_t sip[4];
 	uint8_t dip[4];
-	uint8_t *options;  // Up to 40 bytes
-	// TODO: Here comes tcp/udp header
+	union {
+		tcp_t tcp;
+		udp_t udp;
+	};
 };
-typedef struct ip_header ip_t;
+typedef struct ip_packet ip_t;
 
 struct ethernet_frame {
 	uint8_t smac[6];
@@ -51,7 +75,10 @@ struct ethernet_frame {
 };
 typedef struct ethernet_frame eth_t;
 
-eth_t wrap_packet(const u_char *packet);
+eth_t	wrap_packet_eth(const u_char *packet);
+ip_t	wrap_packet_ip(const u_char *packet);
+tcp_t	wrap_packet_tcp(const u_char *packet);
+udp_t	wrap_packet_udp(const u_char *packet);
 
 int main(int argc, char **argv) {
 	if (argc < 2) {
@@ -117,7 +144,7 @@ int main(int argc, char **argv) {
 			if (res == -1 || res == -2) break;
 #ifdef DEBUG
 			printf("Packet length: %d\n", header->caplen);
-			eth_t ethernet = wrap_packet(packet);
+			eth_t ethernet = wrap_packet_eth(packet);
 #endif
 		}
 		pcap_close(handle);
@@ -131,35 +158,15 @@ int main(int argc, char **argv) {
 }
 
 
-eth_t wrap_packet(const u_char *packet) {
+eth_t wrap_packet_eth(const u_char *packet) {
 	eth_t ret;
 	u_char *cur = const_cast<u_char*>(packet);
 
 	PUSH(cur, ret.smac, sizeof ret.smac);
 	PUSH(cur, ret.dmac, sizeof ret.dmac);
 	PUSH(cur, ret.type, sizeof ret.type);
-
-	uint8_t tmp_8;
-	PUSH(cur, &tmp_8, sizeof tmp_8);
-	ret.data.version = tmp_8 & 0xf0;
-	ret.data.header_length = tmp_8 & 0x0f;
-
-	PUSH(cur, &ret.data.type, sizeof ret.data.type);
-	PUSH(cur, &ret.data.packet_length, sizeof ret.data.packet_length);
-	PUSH(cur, &ret.data.identifier, sizeof ret.data.identifier);
-
-	uint16_t tmp_16;
-	PUSH(cur, &tmp_16, sizeof tmp_16);
-	ret.data.flags = tmp_16 & 0xe000;
-	ret.data.fragment_offset = tmp_16 & 0x1fff;
 	
-	PUSH(cur, &ret.data.ttl, sizeof ret.data.ttl);
-	PUSH(cur, &ret.data.protocol_type, sizeof ret.data.protocol_type);
-	PUSH(cur, &ret.data.checksum, sizeof ret.data.checksum);
-
-	PUSH(cur, ret.data.sip, sizeof ret.data.sip);
-	PUSH(cur, ret.data.dip, sizeof ret.data.dip);
-
+	ret.data = wrap_packet_ip(cur);
 #ifdef DEBUG
 	printf("s-mac: %02x:%02x:%02x:%02x:%02x:%02x\n",
 		ret.smac[0], ret.smac[1], ret.smac[2],
@@ -167,15 +174,77 @@ eth_t wrap_packet(const u_char *packet) {
 	printf("d-mac: %02x:%02x:%02x:%02x:%02x:%02x\n",
 		ret.dmac[0], ret.dmac[1], ret.dmac[2],
 		ret.dmac[3], ret.dmac[4], ret.dmac[5]);
-	printf("s-ip: %u.%u.%u.%u\n",
-		ret.data.sip[0], ret.data.sip[1],
-		ret.data.sip[2], ret.data.sip[3]);
-	printf("d-ip: %u.%u.%u.%u\n",
-		ret.data.dip[0], ret.data.dip[1],
-		ret.data.dip[2], ret.data.dip[3]);
-	printf("protocol: %s\n", 
-		(ret.data.protocol_type == TCP_NUMBER) ? "TCP" : "UDP");
 #endif
+	return ret;
+}
+
+
+ip_t wrap_packet_ip(const u_char *packet) {
+	ip_t ret;
+	u_char *cur = const_cast<u_char*>(packet);
+
+	uint8_t tmp_8;
+	PUSH(cur, &tmp_8, sizeof tmp_8);
+	ret.version = tmp_8 & 0xf0;
+	ret.header_length = tmp_8 & 0x0f;
+
+	PUSH(cur, &ret.type, sizeof ret.type);
+	PUSH(cur, &ret.packet_length, sizeof ret.packet_length);
+	PUSH(cur, &ret.identifier, sizeof ret.identifier);
+
+	uint16_t tmp_16;
+	PUSH(cur, &tmp_16, sizeof tmp_16);
+	ret.flags = tmp_16 & 0xe000;
+	ret.fragment_offset = tmp_16 & 0x1fff;
+	
+	PUSH(cur, &ret.ttl, sizeof ret.ttl);
+	PUSH(cur, &ret.protocol_type, sizeof ret.protocol_type);
+	PUSH(cur, &ret.checksum, sizeof ret.checksum);
+
+	PUSH(cur, ret.sip, sizeof ret.sip);
+	PUSH(cur, ret.dip, sizeof ret.dip);
+
+#ifdef DEBUG
+	printf("s-ip: %u.%u.%u.%u\n",
+		ret.sip[0], ret.sip[1],
+		ret.sip[2], ret.sip[3]);
+	printf("d-ip: %u.%u.%u.%u\n",
+		ret.dip[0], ret.dip[1],
+		ret.dip[2], ret.dip[3]);
+#endif
+	switch (ret.protocol_type) {
+	case TCP_NUMBER:
+		ret.tcp = wrap_packet_tcp(cur);
+		break;
+	case UDP_NUMBER:
+		break;
+	default:
+		break;
+	}
+
+	return ret;
+}
+
+
+tcp_t wrap_packet_tcp(const u_char *packet) {
+	tcp_t ret;
+	u_char *cur = const_cast<u_char*>(packet);
+
+	PUSH(cur, &ret.sp, sizeof ret.sp);
+	PUSH(cur, &ret.dp, sizeof ret.dp);
+
+#ifdef DEBUG
+	printf("s-port: %u\n", ntohs(ret.sp));
+	printf("d-port: %u\n", ntohs(ret.dp));
+#endif
+	return ret;
+}
+
+
+udp_t wrap_packet_udp(const u_char *packet) {
+	udp_t ret;
+	u_char *cur = const_cast<u_char*>(packet);
+
 	return ret;
 }
 
