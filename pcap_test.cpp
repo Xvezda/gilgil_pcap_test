38ced222a793c90fb2d0cb861d6506f42176cdfd
Xvezda <xvezda@naver.com>
2019-07-16 20:13:19 -0700

    Add device and file handler

--- pcap_test.cpp
+++ pcap_test.cpp
@@ -3,24 +3,39 @@
  */
 #include <cstdio>
 #include <cstdlib>
+#include <cstring>
+
+#include <pcap.h>
 
 #include <unistd.h>
+#include <linux/limits.h>
 
 
 using std::printf;
+using std::strlen;
+using std::memcpy;
 
 int main(int argc, char **argv) {
 	if (argc < 2) {
 		printf("Usage: %s [interface] "
 			"[-f packet_dumpfile_path]\n", argv[0]);
 
-		return 1;
+		return EXIT_FAILURE;
 	}
-	int o;
+	int o = 0;
+	char *filepath = nullptr;
 	while ((o = getopt(argc, argv, "f:")) != -1) {
 		switch (o) {
-		case 'f':
+		case 'f': {
+			size_t optlen = strlen(optarg);
+			if (optlen > PATH_MAX) {
+				printf("File path argument too long.");
+				return EXIT_FAILURE;
+			}
+			filepath = new char[optlen];
+			memcpy(filepath, optarg, optlen);
 			break;
+		}
 		case '?':
 		default:
 			switch (optopt) {
@@ -32,13 +47,20 @@ int main(int argc, char **argv) {
 				printf("Unknown option: %c\n", o);
 				break;
 			}
-			return 1;
+			return EXIT_FAILURE;
 
 			break;
 		}
 	}
-	printf("Test: %s\n", argv[1]);
+	char *device = nullptr;
+	pcap_t *handle = nullptr;
 
+	if (filepath == nullptr) {
+		printf("Device: %s\n", argv[1]);
+	} else {
+		printf("File Path: %s\n", filepath);
+		delete filepath;
+	}
 	return EXIT_SUCCESS;
 }
 
