c9cfe2cfeae2a0b263a6639fc421a036de2d3bf0
Xvezda <xvezda@naver.com>
2019-07-16 18:33:39 -0700

    Add getopt

--- pcap_test.cpp
+++ pcap_test.cpp
@@ -1,3 +1,6 @@
+/*
+ * Copyright (c) 2019 Xvezda <https://xvezda.com>
+ */
 #include <cstdio>
 #include <cstdlib>
 
@@ -9,11 +12,32 @@ using std::printf;
 int main(int argc, char **argv) {
 	if (argc < 2) {
 		printf("Usage: %s [interface] "
-			"[-f packet_dump_file]\n", argv[0]);
+			"[-f packet_dumpfile_path]\n", argv[0]);
 
 		return 1;
 	}
-	printf("Test\n");
+	int o;
+	while ((o = getopt(argc, argv, "f:")) != -1) {
+		switch (o) {
+		case 'f':
+			break;
+		case '?':
+		default:
+			switch (optopt) {
+			case 'f':
+				printf("Missing argument to "
+					"packet file path.\n");
+				break;
+			default:
+				printf("Unknown option: %c\n", o);
+				break;
+			}
+			return 1;
+
+			break;
+		}
+	}
+	printf("Test: %s\n", argv[1]);
 
 	return EXIT_SUCCESS;
 }
